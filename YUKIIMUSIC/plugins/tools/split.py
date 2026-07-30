952955adbfd825263985a3aa6ea30eb4fbdf9e80
SQBOT0 <samimsuperboy@gmail.com>
2026-07-30 10:55:17 +0530

    Update split.py

--- YUKIIMUSIC/plugins/tools/split.py
+++ YUKIIMUSIC/plugins/tools/split.py
@@ -1,30 +1,8 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
-import YUKIIMUSIC.yuki_guard
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
 
-from YUKIIMUSIC import app
+from ANIYAXMUSIC import app
 
 @app.on_message(filters.command("split") & filters.reply)
 async def split_file(client: Client, message: Message):
